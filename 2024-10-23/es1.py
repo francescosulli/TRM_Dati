8f763d4aa37984927ba28b0233b8bb3767da4770
francescosulli <franzi2422@gmail.com>
2024-10-23 13:09:49 +0000

    es corretto

--- 2024-10-23/es1.py
+++ 2024-10-23/es1.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import numpy as np
 
-# Creazione di una Pandas Series con 10 numeri interi casuali compresi tra 1 e 100
+#serie
 serie = pd.Series(np.random.randint(1, 101, 10))
 
-# Calcolo della media, deviazione standard e somma
+#calcolo
 media = serie.mean()
 deviazione_standard = serie.std()
 somma = serie.sum()
 
-# Filtraggio dei valori minori di 50
+#confronto
 valori_minori_50 = serie[serie < 50]
 
-# Risultati
+#plot
 print("Serie:\n", serie)
 print("Media:", media)
 print("Deviazione standard:", deviazione_standard)
