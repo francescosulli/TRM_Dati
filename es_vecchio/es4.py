422dc1eeba2a06a1020d9d5f75f286a27c989629
francescosulli <franzi2422@gmail.com>
2025-01-02 16:29:47 +0000

    fine corso

--- es_vecchio/es4.py
+++
@@ -1,6 +0,0 @@
-# Creazione di una tupla
-tupla = ("Mele", "Banane", "Pane", "Latte", "Uova", "Pasta")
-
-# Iterazione sulla tupla con enumerate e stampa della coppia indice/elemento
-for indice, elemento in enumerate(tupla):
-    print(f"Indice {indice}: {elemento}")
